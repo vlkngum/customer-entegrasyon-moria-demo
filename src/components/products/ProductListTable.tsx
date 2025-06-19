d72577127913f9cacdadb1f192c4e95ffd2da74e
Kaytiue7 <Kaytiue7@users.noreply.github.com>
2025-06-19 16:48:59 +0300

    revize

--- src/components/products/ProductListTable.tsx
+++ src/components/products/ProductListTable.tsx
@@ -22,7 +22,10 @@ export default function ProductListTable() {
           className="input"
           placeholder="Stok Kodu, Ürün Adı veya Barkod ile ara..."
         />
-        <button className="bg-blue-600 text-white px-6 py-2 rounded font-semibold">FİLTRELE</button>
+        <button className="bg-blue-600 text-white px-12 py-2 rounded-lg font-semibold flex items-center space-x-2 hover:bg-blue-700 transition-colors duration-200 ">
+                        <FaSearch className="w-4 h-4" />
+                        <span>FİLTRELE</span>
+                    </button>
         <div className="flex-1 flex justify-end gap-2">
           <button className="border px-3 py-1 rounded text-gray-700">Toplu İşlemler</button>
           <button className="border px-3 py-1 rounded text-gray-700">Detaylı Filtrele</button>
